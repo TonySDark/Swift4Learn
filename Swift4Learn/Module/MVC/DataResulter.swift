b4f786b49fdb7555f8efbd2a768d675b183d3357
Tony <tony.song@buyal.com>
2018-06-11 15:07:54 +0800

    更改

--- Swift4Learn/Module/MVC/DataResulter.swift
+++ Swift4Learn/Module/MVC/DataResulter.swift
@@ -14,22 +14,10 @@ class DataResulter: NSObject {
     func getData() {
        //网络获取数据
         //这里写自己的话
-        let words1Arr = ["认真的看着我的眼神～你已经说过早啦",
-                         "有些时候，心有郁结而不能发。",
-                         "就会写东西抒发情感",
-                         "原谅无知的羔羊吧 公主",
-                         "喝水会笑的狗，哇！！",
-                         "只希望你每天都快快乐乐的"]
-        let words2Arr = ["？对啊,😂",
-                         "乖巧的我，认真听你讲话",
-                         "哦~",
-                         "哼(´◔ ‸◔')我这么傲娇，不和你打电话",
-                         "是不是很有喜感,像你一样",
-                         "我要静静地抱住我圆滚滚的身体"]
-        let happyWordsArr1 = ["美美，Happy BirthDay!",
-                              "为每天的你献上祝福！"]
-        let happyWordsArr2 = ["为美丽的公主献上祝福！",
-                              "为平凡的你献上祝福！"]
+        let words1Arr = ["啦啦"]
+        let words2Arr = ["啊啦"]
+        let happyWordsArr1 = ["为每天的你献上祝福！"]
+        let happyWordsArr2 = ["为平凡的你献上祝福！"]
 
     }
     
